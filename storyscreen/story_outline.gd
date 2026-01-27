b1e989c45d7945abe1893037630bb966a07ffc40
Francis Chen <huan@Franciss-MacBook-Pro.local>
2026-01-27 15:33:24 +0000

    Title And Story screen polish

--- storyscreen/story_outline.gd
+++ storyscreen/story_outline.gd
@@ -2,7 +2,7 @@ extends RichTextLabel
 
 @export var full_text := "[font_size=32][center]You are the Knight of the Herbonia Realm.
 
-[font_size=24]Your princess has been abducted by the LIME - [i]Lords of Time.[/i] The King has appointed [b]you[/b] to save her from their clutches.
+[font_size=24]Your princess has been abducted by the LIME - [b]Lords of Time[/b]. The King has appointed [b]you[/b] to save her from their clutches.
 
 Your journey starts here.[/font_size][/center]"
 
