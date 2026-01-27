e57df74b246ef10b774b2ed7a6c2316270d7216e
Francis Chen <huan@Franciss-MacBook-Pro.local>
2026-01-27 14:59:04 +0000

    Title and Story screen

---
+++ storyscreen/story_outline.gd
@@ -0,0 +1,23 @@
+extends RichTextLabel
+
+@export var full_text := "[font_size=32][center]You are the Knight of the Herbonia Realm.
+
+[font_size=24]Your princess has been abducted by the LIME - [i]Lords of Time.[/i] The King has appointed [b]you[/b] to save her from their clutches.
+
+Your journey starts here.[/font_size][/center]"
+
+@export var time_delay := 0.04
+@onready var continue_button := $"../ContinueButton"
+
+func _ready():
+	text = ""
+	type_text()
+	continue_button.visible = false
+	
+
+func type_text() -> void:
+	for i in full_text.length():
+		text += full_text[i]
+		await get_tree().create_timer(time_delay).timeout
+	continue_button.visible = true
+	
